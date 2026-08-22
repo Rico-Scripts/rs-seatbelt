5c2e3c91aeca217e8e3ddfe5fda89a991984e68f
rico <r03292760@gmail.com>
2026-08-23 01:29:04 +0200

    Release rs-seatbelt 1.0.0

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.Command = 'gordel'
+Config.Key = 'B'
+Config.DisableExitWhileBuckled = true
+Config.EnableEjection = true
+Config.MinimumEjectSpeed = 65.0
+Config.MinimumSpeedDrop = 35.0
+Config.EjectCooldown = 3000
+Config.RagdollMilliseconds = 3500
+Config.EjectDamage = 20
+Config.IgnoredVehicleClasses = {
+    [8] = true,
+    [13] = true,
+    [14] = true,
+    [15] = true,
+    [16] = true,
+    [21] = true,
+}
