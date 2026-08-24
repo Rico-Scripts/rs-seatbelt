9991dab7ed8f13c13e35dd663a7dfdab8ec53c2b
rico <r03292760@gmail.com>
2026-08-24 14:07:32 +0200

    Release rs-seatbelt 1.1.0 with manual motorcycle helmet

--- config.lua
+++ config.lua
@@ -2,6 +2,12 @@ Config = {}
 
 Config.Command = 'gordel'
 Config.Key = 'B'
+Config.EnableMotorcycleHelmet = true
+Config.PreventAutomaticMotorcycleHelmet = true
+Config.RemoveHelmetOnExit = true
+Config.MotorcycleClass = 8
+Config.HelmetFlag = 4096
+Config.HelmetTexture = -1
 Config.DisableExitWhileBuckled = true
 Config.EnableEjection = true
 Config.MinimumEjectSpeed = 65.0
